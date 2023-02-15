ab409c5bf35d01563b4f7234f55e7f188261f96d
Shaun <shaunkleyn@gmail.com>
2023-02-15 20:20:33 +0200

    change log path

--- upcoming-shows.py
+++ upcoming-shows.py
@@ -35,7 +35,7 @@ library_name = plex_library
 #############
 ## LOGGING ##
 #############
-log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../upcoming_shows.log')
+log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '/upcoming_shows.log')
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 # create logger
 logger = logging.getLogger('')
