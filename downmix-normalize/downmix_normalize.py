53007c4b2574623203591ff0d6c4826051632df1
gwatch <gwatch@example.com>
2023-03-15 10:27:11 +0200

    gwatch auto-commit

--- downmix-normalize/downmix_normalize.py
+++ downmix-normalize/downmix_normalize.py
@@ -53,16 +53,16 @@ for root, dirs, files in os.walk(directory):
                 logger.info(f'Title: {title}')
                 
             except:
-                logger.fata(f'Could not retrieve audio details for file {file}')
+                logger.fatal(f'Could not retrieve audio details for file {file}')
                 sys.exit(1)
             
             # extract audio to wav and apply filter if necessary
             if channels > 2:
-                print("Downmixing " + str(channels) + " and extracting WAV")
+                logger.info(f'Downmixing {str(channels)} and extracting WAV')
                 # subprocess.run(["ffmpeg", "-hide_banner", "-y", "-i", file, "-af", "pan=stereo|FL=0.25*FL+FC+0.6*LFE|FR=0.25*FR+FC+0.6*LFE", "-vn", file + "-ffmpeg.wav"])
                 subprocess.run(["ffmpeg", "-hide_banner", "-y", "-i", file, "-af", "pan=stereo|FL=1.0*FL+0.707*FC+0.707*SL+0.707*LFE|FR=1.0*FR+0.707*FC+0.707*SR+0.707*LFE", "-vn", file + "-ffmpeg.wav"])
             elif (channels == 2 and codec != "A_AAC-2"):
-                print("Extracting WAV")
+                logger.info("Extracting WAV")
                 subprocess.run(["ffmpeg", "-hide_banner", "-y", "-i", file, "-vn", file + "-ffmpeg.wav"])
     
             # check if -ffmpeg.wav file exists
